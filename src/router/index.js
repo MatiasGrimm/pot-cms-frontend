f2a3b3f41130ab0cd8c9fe4ed92ea9ced5d127cf
TavsMalling <67231672+TavsMalling@users.noreply.github.com>
2022-11-16 12:33:16 +0100

    Deleted unused files

--- src/router/index.js
+++ src/router/index.js
@@ -15,168 +15,165 @@ const routes = [
   {
     path: '/admin',
     component: () => import('../views/AdminPanel.vue'),
-    redirect: '/admin/users',
-    meta: {
-      requiresManager: true,
-    },
+    // redirect: '/admin/users',
     children: [
-      {
-        path: 'users',
-        name: 'UserList',
-        component: () => import('../components/Admin/UserList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'UserDetails',
-            component: () => import('../components/Admin/Details/UserDetails.vue'),
-          },
-        ],
-      },
-      {
-        path: 'companies',
-        name: 'CompanyList',
-        component: () => import('../components/Admin/CompanyList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'CompanyDetails',
-            component: () => import('@/components/Admin/Details/CompanyDetails.vue'),
-          },
-        ],
-      },
-      {
-        path: 'sqlScripts',
-        name: 'SqlScriptList',
-        component: () => import('../components/Admin/SqlScriptList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'SQLDetails',
-            component: () => import('@/components/Admin/Details/SqlScriptDetails.vue'),
-          },
-        ],
-      },
-      {
-        path:'configuration',
-        name:'Configuration',
-        component: () => import('@/views/Configuration.vue'),
-        redirect: {name: 'ConfigurationList'},
-        children: [
-          {
-            path:'list',
-            name:'ConfigurationList',
-            component: () => import('@/components/Admin/ConfigurationList.vue')
-          },
-          {
-            path:':Id',
-            name:'ConfigurationDetails',
-            component: () => import('@/components/Admin/Details/ConfigurationDetails.vue')
-          }
-        ]
-      },
-      {
-        path: 'configurationFiles',
-        name: 'ConfigurationFileList',
-        component: () => import('../components/Admin/ConfigurationFileList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'ConfigurationFileDetails',
-            component: () => import('@/components/Admin/Details/ConfigurationFileDetails.vue'),
-          },
-        ],
-      },
-      {
-        path: 'applications',
-        name: 'ApplicationList',
-        component: () => import('@/components/Admin/ApplicationList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'ApplicationDetails',
-            component: () => import('@/components/Admin/Details/ApplicationDetails.vue'),
-          },
-        ],
-      },
-      {
-        path: 'applicationReleases',
-        name: 'ApplicationReleaseList',
-        component: () => import('@/components/Admin/ApplicationReleaseList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'ApplicationReleaseDetails',
-            component: () => import('@/components/Admin/Details/ApplicationReleaseDetails.vue'),
-          },
-        ],
-      },
-      {
-        path: 'applicationElements',
-        name: 'ApplicationElementList',
-        component: () => import('@/components/Admin/ApplicationElementList.vue'),
-        children: [
-          {
-            path: ':Id',
-            name: 'ApplicationElementDetails',
-            component: () => import('@/components/Admin/Details/ApplicationElementsDetails.vue'),
-          },
-        ],
-      },
-      {
-        path: 'license',
-        name:'License',
-        component: ()=> import('@/views/License.vue'),
-        redirect: { name: 'LicenseList' },
-        children:[
-          {
-            path:'list',
-            name:'LicenseList',
-            component: () => import('@/components/Admin/LicenseList.vue')
-          },
-          {
-            path:':Id',
-            name:'LicenseDetails',
-            component: () => import('@/components/Admin/Details/LicenseDetails.vue')
-          }
-        ]
-      },
-      {
-        path: 'checklog',
-        name:'UpdateCheckLog',
-        component: ()=> import('@/views/UpdateCheckLog.vue'),
-        redirect: { name: 'CheckLogList' },
-        children:[
-          {
-            path:'list',
-            name:'CheckLogList',
-            component: () => import('@/components/Admin/CheckLogList.vue')
-          },
-          {
-            path:':Id',
-            name:'CheckLogDetails',
-            component: () => import('@/components/Admin/Details/CheckLogDetails.vue')
-          }
-        ]
-      },
-      {
-        path: 'downloadlog',
-        name:'UpdateDownloadLog',
-        component: ()=> import('@/views/UpdateDownloadLog.vue'),
-        redirect: { name: 'DownloadLogList' },
-        children:[
-          {
-            path:'list',
-            name:'DownloadLogList',
-            component: () => import('@/components/Admin/DownloadLogList.vue')
-          },
-          {
-            path:':Id',
-            name:'DownloadLogDetails',
-            component: () => import('@/components/Admin/Details/DownloadLogDetails.vue')
-          }
-        ]
-      }
-    ],
+    //   {
+    //     path: 'users',
+    //     name: 'UserList',
+    //     component: () => import('../components/Admin/UserList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'UserDetails',
+    //         component: () => import('../components/Admin/Details/UserDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: 'companies',
+    //     name: 'CompanyList',
+    //     component: () => import('../components/Admin/CompanyList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'CompanyDetails',
+    //         component: () => import('@/components/Admin/Details/CompanyDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: 'sqlScripts',
+    //     name: 'SqlScriptList',
+    //     component: () => import('../components/Admin/SqlScriptList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'SQLDetails',
+    //         component: () => import('@/components/Admin/Details/SqlScriptDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path:'configuration',
+    //     name:'Configuration',
+    //     component: () => import('@/views/Configuration.vue'),
+    //     redirect: {name: 'ConfigurationList'},
+    //     children: [
+    //       {
+    //         path:'list',
+    //         name:'ConfigurationList',
+    //         component: () => import('@/components/Admin/ConfigurationList.vue')
+    //       },
+    //       {
+    //         path:':Id',
+    //         name:'ConfigurationDetails',
+    //         component: () => import('@/components/Admin/Details/ConfigurationDetails.vue')
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: 'configurationFiles',
+    //     name: 'ConfigurationFileList',
+    //     component: () => import('../components/Admin/ConfigurationFileList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'ConfigurationFileDetails',
+    //         component: () => import('@/components/Admin/Details/ConfigurationFileDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: 'applications',
+    //     name: 'ApplicationList',
+    //     component: () => import('@/components/Admin/ApplicationList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'ApplicationDetails',
+    //         component: () => import('@/components/Admin/Details/ApplicationDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: 'applicationReleases',
+    //     name: 'ApplicationReleaseList',
+    //     component: () => import('@/components/Admin/ApplicationReleaseList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'ApplicationReleaseDetails',
+    //         component: () => import('@/components/Admin/Details/ApplicationReleaseDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: 'applicationElements',
+    //     name: 'ApplicationElementList',
+    //     component: () => import('@/components/Admin/ApplicationElementList.vue'),
+    //     children: [
+    //       {
+    //         path: ':Id',
+    //         name: 'ApplicationElementDetails',
+    //         component: () => import('@/components/Admin/Details/ApplicationElementsDetails.vue'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: 'license',
+    //     name:'License',
+    //     component: ()=> import('@/views/License.vue'),
+    //     redirect: { name: 'LicenseList' },
+    //     children:[
+    //       {
+    //         path:'list',
+    //         name:'LicenseList',
+    //         component: () => import('@/components/Admin/LicenseList.vue')
+    //       },
+    //       {
+    //         path:':Id',
+    //         name:'LicenseDetails',
+    //         component: () => import('@/components/Admin/Details/LicenseDetails.vue')
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: 'checklog',
+    //     name:'UpdateCheckLog',
+    //     component: ()=> import('@/views/UpdateCheckLog.vue'),
+    //     redirect: { name: 'CheckLogList' },
+    //     children:[
+    //       {
+    //         path:'list',
+    //         name:'CheckLogList',
+    //         component: () => import('@/components/Admin/CheckLogList.vue')
+    //       },
+    //       {
+    //         path:':Id',
+    //         name:'CheckLogDetails',
+    //         component: () => import('@/components/Admin/Details/CheckLogDetails.vue')
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: 'downloadlog',
+    //     name:'UpdateDownloadLog',
+    //     component: ()=> import('@/views/UpdateDownloadLog.vue'),
+    //     redirect: { name: 'DownloadLogList' },
+    //     children:[
+    //       {
+    //         path:'list',
+    //         name:'DownloadLogList',
+    //         component: () => import('@/components/Admin/DownloadLogList.vue')
+    //       },
+    //       {
+    //         path:':Id',
+    //         name:'DownloadLogDetails',
+    //         component: () => import('@/components/Admin/Details/DownloadLogDetails.vue')
+    //       }
+    //     ]
+    //   }
+   ],
   },
   {
     path: '/resetpassword',
