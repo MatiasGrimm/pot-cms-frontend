7644dc33a835d44ade20f108f95bc904c7b26076
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2022-11-28 19:47:20 +0100

    Add RBtn to project

---
+++ src/components/RBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-btn class="px-4" v-bind="attributes" v-on="$listeners">
+    <slot />
+  </v-btn>
+</template>
+<script>
+export default {
+  name: 'r-btn',
+  props: {
+    soft: Boolean,
+    action: Boolean,
+    creation: Boolean,
+    delete: Boolean,
+    text: Boolean,
+  },
+  computed: {
+    attributes() {
+      return {
+        color: this.soft ? null : this.action ? 'activeButton' : this.creation ? 'success' : this.delete ? 'error' : this.text ? 'info' : 'null',
+        class: this.action ? 'white--text' : null,
+        depressed: false,
+        elevation: this.$vuetify.theme.dark && !this.text ? 2 : 0,
+        text: this.text,
+        ...this.$attrs,
+      };
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import RBtn from './components/RBtn.vue';
 import vuetify from './plugins/vuetify';
 import SearchTextField from '@/components/SearchTextField.vue';
 
-
 Vue.config.productionTip = false;
 
-
+Vue.component('r-btn', RBtn);
 Vue.component('search-text-field', SearchTextField);
 
 new Vue({
